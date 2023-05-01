aca057cbe582d2157169fca3da242a25e634529f
Tanjir-Hasan <ihasan.tanjir@gmail.com>
2023-05-02 03:06:55 +0600

    fixed images problem

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.get('/data', (req, res) => {
     res.send(data);
 });
 
+app.get('/data/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const singleData = data.find(item => item.chef_id === id);
+    res.send(singleData);
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
