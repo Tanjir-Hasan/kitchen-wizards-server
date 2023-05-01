dd192c5f7f9c35fb498bcba77f5adcb6fa4c1565
Tanjir-Hasan <ihasan.tanjir@gmail.com>
2023-05-02 01:08:03 +0600

    cors added

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const port = process.env.port || 5000;
+app.use(cors());
 
 const data = require('./data/data.json');
-
-
 
 app.get('/', (req, res) => {
   res.send('kitten is on');
@@ -13,8 +12,8 @@ app.get('/', (req, res) => {
 
 app.get('/data', (req, res) => {
     res.send(data);
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
